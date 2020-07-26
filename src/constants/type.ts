9f81680496c52af16cc106a4baf6594152ac54b5
chienvu123 <chienvu1308@gmail.com>
2020-07-26 17:05:40 +0700

    add UserType + MyRequest

--- src/constants/type.ts
+++ src/constants/type.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express';
 export const increment = {
   registerAccount: 'userCounter',
 };
@@ -9,3 +10,20 @@ export const accType = {
   system: 'USER_PASSWORD',
   social: 'SOCIAL',
 };
+export interface MyRequest extends Request {
+  user?: {
+    userId: number;
+    roles?: any;
+    name: string;
+  }; // or any other type
+}
+export interface UserType {
+  _id: Number;
+  name: String;
+  avatar: String; // luu file, sau do de duong dan vao day,
+  address: String;
+  phone: String;
+  url: String;
+  email?: String;
+  roles?: any;
+}
